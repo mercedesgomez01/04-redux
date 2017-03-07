fa2dfcf07733ffdc795c235e9f1fb91695d65699
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-03-07 11:35:26 -0500

    got rid of commit

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
-var chalk = require('chalk')
+//var chalk = require('chalk');
+var nunjucks = require('nunjucks');
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks // point nunjucks to the proper directory for templates
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+nunjucks.configure('views', {noCache: true});
+
+nunjucks.render('index.html', locals, function(err, output) {
+	if (err) console.log(err);
+    console.log(output);
+});
 
 app.use(function (req, res, next) {
   //console.log(chalk.pink('Time:'), Date.now())
@@ -13,7 +33,8 @@ app.get('/modernism', function (req, res) {
 })
 
 app.get('/', function(req, res) {
-  res.send('Welcome, Friend!');
+  //res.send('Welcome, Friend!');
+  res.render('index', {title: 'Hall of Fame', people: people} );
 });
 
 app.get('/news', function(req, res) {
