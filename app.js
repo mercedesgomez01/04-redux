d00b6ba5aa3b6a19d7cd4ff504b3e8aecb360fd2
mercedesgomez01 <mercedesgomez01@gmail.com>
2017-03-07 10:51:06 -0500

    repaired twitter folder

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+var chalk = require('chalk')
+
+app.use(function (req, res, next) {
+  //console.log(chalk.pink('Time:'), Date.now())
+  console.log('Time:', Date.now())
+  next()
+})
+
+app.get('/modernism', function (req, res) {
+  console.log('modernism')
+})
 
 app.get('/', function(req, res) {
   res.send('Welcome, Friend!');
